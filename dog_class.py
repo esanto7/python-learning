a2000281297c3f91f88c2faf047251c179ca04de
Brandon <brandon.santos@students.mq.edu.au>
2023-11-10 16:16:55 +1100

    new file

---
+++ dog_class.py
@@ -0,0 +1,20 @@
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    # instance method
+    def __str__(self):
+            return f"{self.name} is {self.age} years old."
+    
+    # Another instance method
+    def speak(self, sound):
+         return f"{self.name} says {sound}."
+    
+miles = Dog("Miles", 4)
+
+print(miles)
+
+print(miles.speak("woof"))
